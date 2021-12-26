2d2c5fffbd0546c4b511d9e916b3f2572c54b112
sᴏᴜʀᴄʀ ʟᴏᴅᴋɴ ♰ <95194229+LODKN@users.noreply.github.com>
2021-12-26 03:07:35 +0300

    Update LoDknV2.lua

--- LoDknV2.lua
+++ LoDknV2.lua
@@ -10408,7 +10408,7 @@ end
 if text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/TEAMLODKNTEAM/TEAMLODKNFiles/master/TEAMLODKNFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/LoDkn/LoDknV2Files/master/LoDknV2Files/"..FileName)
 if Res == 200 then
 os.execute("rm -fr Files/"..FileName)
 send(msg.chat_id_, msg.id_,"\n♰︙الملف ♰ *"..FileName.."*\n♰︙تم تعطيله وحذفه من البوت بنجاح") 
@@ -10420,7 +10420,7 @@ end
 if text and text:match("^(تفعيل ملف) (.*)(.lua)$") then
 local FileGet = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local FileName = FileGet[2]..'.lua'
-local GetJson, Res = https.request("https://raw.githubusercontent.com/TEAMLODKNTEAM/TEAMLODKNFiles/master/TEAMLODKNFiles/"..FileName)
+local GetJson, Res = https.request("https://raw.githubusercontent.com/LoDkn/LoDknV2Files/master/LoDknV2Files/"..FileName)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..FileName,'w+')
 ChekAuto:write(GetJson)
